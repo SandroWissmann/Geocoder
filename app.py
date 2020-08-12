dff15ef557ec185daff8018df99504f039bd6c69
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-12 15:48:19 +0200

    feat: adds saving of uploaded file

--- app.py
+++ app.py
@@ -1,16 +1,38 @@
 from flask import Flask, render_template, request, send_file
+from werkzeug.utils import secure_filename
+import pandas
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
+    """Shows the main page of the webapp"""
     return render_template("index.html")
 
 
 @app.route("/success", methods=["POST"])
 def success():
+    """
+    If a valid CSV file is uploaded generate add geometrical 
+    information to the file and return it
+    """
     if request.method == "POST":
+        file = request.files["file"]
+        if not file.filename.endswith(".csv"):
+            return render_template("index.html")
+
+        filename = "uploaded" + file.filename
+        file.save(secure_filename(filename))
+
+        # open file with pandas
+
+        # check for address or Address
+
+        # add latitude and longitude if available
+
+        # save yourfile.csv
+
         return render_template(
             "index.html", button_download="button_download.html"
         )
