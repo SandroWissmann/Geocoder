c8490478f196eee07eacf4444e856214109a3405
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-12 16:54:09 +0200

    feat: adds calculation of coordinates

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, send_file
 from werkzeug.utils import secure_filename
+from geopy.geocoders import ArcGIS
 import pandas
 
 app = Flask(__name__)
@@ -26,16 +27,38 @@ def success():
         file.save(secure_filename(filename))
 
         # open file with pandas
+        df = pandas.read_csv(filename)
 
         # check for address or Address
+        column_name = get_column_name(df, ["address", "Address"])
+        if not column_name:
+            return render_template("index.html")
 
         # add latitude and longitude if available
+        nominator = ArcGIS()
+        df["Coordinates"] = df[column_name].apply(nominator.geocode)
+
+        df["Latitude"] = df["Coordinates"].apply(
+            lambda x: x.latitude if x != None else None
+        )
+        df["Longitude"] = df["Coordinates"].apply(
+            lambda x: x.longitude if x != None else None
+        )
+        df.drop("Coordinates", axis=1)
+        print(df)
 
         # save yourfile.csv
 
         return render_template(
             "index.html", button_download="button_download.html"
         )
+
+
+def get_column_name(dataframe, possible_column_names):
+    for possible_column_name in possible_column_names:
+        if possible_column_name in dataframe.columns:
+            return possible_column_name
+    return None
 
 
 @app.route("/download")
