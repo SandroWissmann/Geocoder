b0c2bf3df62d21b0ff959e44232e24278558144b
SandroWissmann <sandro.wissmann@googlemail.com>
2020-08-13 10:18:03 +0200

    refactor: move check for file type csv to html

--- app.py
+++ app.py
@@ -21,49 +21,51 @@ def success():
     """
     if request.method == "POST":
         file = request.files["file"]
-        if not file.filename.endswith(".csv"):
+
+        try:
+            filename = "uploaded" + file.filename
+            file.save(secure_filename(filename))
+
+            # open file with pandas
+            df = pandas.read_csv(filename)
+
+            remove(filename)
+
+            # check for address or Address
+            column_name = get_column_name(df, ["address", "Address"])
+            if not column_name:
+                return render_template(
+                    "index.html",
+                    tables=[],
+                    text_invalid_file="text_invalid_file.html",
+                )
+
+            # add latitude and longitude if available
+            nominator = ArcGIS(timeout=None)
+            df["Coordinates"] = df[column_name].apply(nominator.geocode)
+            df["Latitude"] = df["Coordinates"].apply(
+                lambda x: x.latitude if x != None else None
+            )
+            df["Longitude"] = df["Coordinates"].apply(
+                lambda x: x.longitude if x != None else None
+            )
+            df = df.drop("Coordinates", axis=1)
+
+            # save yourfile.csv
+            df.to_csv("yourfile.csv")
+
+            return render_template(
+                "index.html",
+                tables=[df.to_html(classes="result_table", header="true")],
+                button_download="button_download.html",
+            )
+
+        except Exception as e:
             return render_template(
                 "index.html",
                 tables=[],
                 text_invalid_file="text_invalid_file.html",
             )
-
-        filename = "uploaded" + file.filename
-        file.save(secure_filename(filename))
-
-        # open file with pandas
-        df = pandas.read_csv(filename)
-
-        remove(filename)
-
-        # check for address or Address
-        column_name = get_column_name(df, ["address", "Address"])
-        if not column_name:
-            return render_template(
-                "index.html",
-                tables=[],
-                text_invalid_file="text_invalid_file.html",
-            )
-
-        # add latitude and longitude if available
-        nominator = ArcGIS(timeout=None)
-        df["Coordinates"] = df[column_name].apply(nominator.geocode)
-        df["Latitude"] = df["Coordinates"].apply(
-            lambda x: x.latitude if x != None else None
-        )
-        df["Longitude"] = df["Coordinates"].apply(
-            lambda x: x.longitude if x != None else None
-        )
-        df = df.drop("Coordinates", axis=1)
-
-        # save yourfile.csv
-        df.to_csv("yourfile.csv")
-
-        return render_template(
-            "index.html",
-            tables=[df.to_html(classes="result_table", header="true")],
-            button_download="button_download.html",
-        )
 
 
 def get_column_name(dataframe, valid_column_names):
